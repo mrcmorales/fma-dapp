c7909881fde0c1eec5a615ace66b6d6dcd859795
Marc Morales <marcmorales83@gmail.com>
2021-01-18 22:39:19 +0100

    social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import { FaTelegram } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
+import {FaTelegram} from "react-icons/fa";
+import {AiFillTwitterCircle} from "react-icons/ai";
+import {FaGithub} from "react-icons/fa";
+import {AiFillMediumCircle} from "react-icons/ai";
 import logo from './../assets/images/logo/flama.png';
 import Navbar from "react-bootstrap/Navbar";
 
@@ -11,19 +13,27 @@ class Footer extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-6 social">
-                            <a href="https://t.me/FlamaToken" title="Flamanet Telegram" target="_blank" rel="noreferrer">
-                                <FaTelegram color="white" />
+                            <a href="https://t.me/FlamaToken" title="Telegram" target="_blank"
+                               rel="noreferrer">
+                                <FaTelegram color="white"/>
                             </a>
-                            <a href="https://twitter.com/FlamaToken" title="Flamanet Twitter" target="_blank" rel="noreferrer">
-                                <FaTwitter color="white" />
+                            <a href="https://twitter.com/FlamaToken" title="Twitter" target="_blank"
+                               rel="noreferrer">
+                                <AiFillTwitterCircle color="white"/>
                             </a>
-
-
+                            <a href="https://github.com/FlamaToken" title="Github" target="_blank"
+                               rel="noreferrer">
+                                <FaGithub color="white"/>
+                            </a>
+                            <a href="https://medium.com/@FlamaToken" title="Medium" target="_blank"
+                               rel="noreferrer">
+                                <AiFillMediumCircle color="white"/>
+                            </a>
                         </div>
                         <div className="col-sm-6 text-right link">
-                            <a href="https://flamanet.io" title="Flamanet" target="_blank" color="white" rel="noreferrer">
+                            <a href="https://flamanet.io" title="Flamanet" target="_blank" color="white"
+                               rel="noreferrer">
                                 <img src={logo} width="15" className="d-inline-block align-top" alt="flama"/>
-
                                 flamanet.io
                             </a>
                         </div>
